3b4b9939db9ed74b2104409e92395b2f7de06ace
Sviatoslav6239 <svf121209@gmail.com>
2025-02-19 09:13:32 +0100

    feat: add Task model with fields for title, description, completion status, and user association

--- taskapp/models.py
+++ taskapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from  auth_app.models import CustomUser
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    stage = models.IntegerField(default=0)
+    deadline = models.DateTimeField()
+    user = models.ForeignKey('auth.User', related_name='tasks', on_delete=models.CASCADE)
+    access = models.ManyToManyField('auth.User', related_name='shared_tasks')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
